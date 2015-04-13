322a43d97a47ac3d7f96ba6a48315549631ed1f8
Zylius <zylius1@gmail.com>
2015-04-13 15:14:02 +0300

    Added user panel, bucket.

---
+++ src/Zylius/ParduotuveBundle/Form/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Zylius\ParduotuveBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Zylius\ParduotuveBundle\Entity\User;
+
+class UserType extends AbstractType
+{
+    /** @var  User */
+    private $user;
+
+    public function __construct(User $caller)
+    {
+        $this->user = $caller;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email');
+
+        if (in_array('ROLE_SUPER_ADMIN', $this->user->getRoles())) {
+            $builder->add('roles');
+        }
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Zylius\ParduotuveBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'zylius_parduotuvebundle_user';
+    }
+}
